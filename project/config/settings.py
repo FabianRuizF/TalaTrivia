95e06b67ad95d681c50bd146132275b26cc650c3
FabianRuizF <fabiancrruiz@gmail.com>
2024-11-23 00:37:51 +0000

    Fix invalid Setting intialization for config/settings.py

--- project/config/settings.py
+++ project/config/settings.py
@@ -6,22 +6,20 @@ class Settings(BaseSettings):
     database_url: str
     secret_key: str
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
-
 def get_settings(env):
-    settings = Settings()
+#    settings = Settings()
 
     if env == "test":
-        env_file_path = os.path.join(os.getcwd(), "test_env.env")
+        env_file_path = "test_env.env"
     elif env == "prod":
-        env_file_path = os.path.join(os.getcwd(), ".env")
+        env_file_path = ".env"
     else:
         raise ValueError("Invalid environment specified")
-
     try:
         load_dotenv(dotenv_path=env_file_path)
+        database_url = os.getenv('DATABASE_URL')
+        secret_key = os.getenv("SECRET_KEY")
+        setting = Settings(database_url=database_url, secret_key=secret_key)
         return Settings()
     except FileNotFoundError:
         raise ValueError(f"Environment file {env_file_path} not found")
