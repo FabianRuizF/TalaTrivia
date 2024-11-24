9649e8ffb76c0fbda8dc5fe012658e0620fbbce7
FabianRuizF <fabiancrruiz@gmail.com>
2024-11-24 16:14:55 +0000

    Add trivia_user model. Remove unusued code

---
+++ project/models/trivia_user.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from .base import Base
+
+
+class TriviaUser(Base):
+    __tablename__ = "trivia_user"
+    trivia_id = Column(Integer,ForeignKey('trivia.trivia_id'), primary_key=True)
+    question_id = Column(Integer,ForeignKey('user.user_id'), primary_key=True)
