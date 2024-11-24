9b3cc0e23f21d52117760967c66b6096964666c1
FabianRuizF <fabiancrruiz@gmail.com>
2024-11-24 16:15:26 +0000

    Remove commented unused code

--- project/startup_database.py
+++ project/startup_database.py
@@ -5,6 +5,7 @@ from models.user import User
 from models.question import Question
 from models.trivia import Trivia
 from models.trivia_question import TriviaQuestion
+from models.trivia_user import TriviaUser
 from sqlalchemy.ext.declarative import declarative_base
 from models.base import Base
 
@@ -13,12 +14,3 @@ print(settings)
 SQLALCHEMY_DATABASE_URL = settings.database_url
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 Base.metadata.create_all(engine)
-#
-## Create Tables
-#User.__table__.create(engine, checkfirst=True)
-#Trivia.__table__.create(engine, checkfirst=True)
-#Question.__table__.create(engine, checkfirst=True)
-#
-## Create junction table
-#from models.trivia_question import TriviaQuestion
-#TriviaQuestion.__table__.create(engine, checkfirst=True)
